db8cb02d33b32c457fe423f74a5b0e0f47235c9d
chirag <maulik2.vasundhara@gmail.com>
2023-08-02 12:33:35 +0530

    change script

--- src/page/productjson.js
+++ src/page/productjson.js
@@ -1,20 +1,21 @@
 export const createJsonLdSchema = (product) => {
-  let description = product.productList?.name +
-    "Item ID: " + product.productList?.attributes['Product ID']?.[0] +
-    "Pattern Type: " + product.productList?.attributes['Pattern Type']?.[0] +
-    "Type: " + product.productList?.attributes.Type?.[0] +
-    "Closure Type: " + product.productList?.attributes['Closure Type']?.[0] +
-    "Details: " + product.productList?.attributes.Details?.[0] +
-    "Waist Line: " + product.productList?.attributes['Waist Line']?.[0] +
-    "Length: " + product.productList?.attributes.Length?.[0] +
-    "Fit Type: " + product.productList?.attributes['Fit Type']?.[0] +
-    "Fabric: " + product.productList?.attributes.Fabric?.[0] +
-    "Material: " + product.productList?.attributes.Material?.[0] +
-    "Composition: " + product.productList?.attributes.Composition?.[0] +
-    "Care Instructions: " + product.productList?.attributes['Care Instructions']?.[0] +
-    "Body: " + product.productList?.attributes.Body?.[0] +
-    "Sheer: " + product.productList?.attributes.Sheer?.[0];
+  // console.log(product,"product");
 
+  function generateDescription(attributes) {
+    let description = "";
+  
+    Object.entries(attributes || {}).forEach(([key, value]) => {
+      if (key === "Product ID") {
+        description += `Item ID: ${value[0]} `;
+      } else {
+        description += `${key}: ${Array.isArray(value) ? value.join(", ") : value} `;
+      }
+    });
+  
+    return description;
+  }
+
+  let description = generateDescription(product.productList?.attributes);
 
     let colors = []; // Create colors as an array instead of an object
     let sizes = [];  // Create sizes as an array instead of an object
@@ -42,7 +43,7 @@ export const createJsonLdSchema = (product) => {
       "@type": "Brand",
       "name": "Clubmall"
     },
-    "gtin8": product.productList?._id,
+    "gtin8": product.productList?.attributes['Product ID']?.[0],
     "url": `https://clubmall.com/product-info/${product.productList?._id}`, //assuming there is an id field in product
     "priceCurrency": "USD",
     "price": product.productList?.individual_price,
